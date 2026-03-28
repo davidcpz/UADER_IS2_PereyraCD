f3b84f6e7dca2ec0904ceb24efae236e043459d8
davidcpz <cdavidp1@hotmail.com>
2026-03-28 05:34:20 -0300

    agrego el rango en el codigo

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -23,12 +23,38 @@ def factorial(num):
 # Verifico si se ingresó un argumento por línea de comandos
 # sys.argv siempre tiene al menos 1 elemento (el nombre del script)
 # por eso, si es menor a 2 significa que NO se ingresó ningún número
-if len(sys.argv) < 2:
+#if len(sys.argv) < 2:
     # Si no se pasó un número como argumento, se solicita al usuario
-    num = int(input("Ingrese un número: "))
-else:
+    #num = int(input("Ingrese un número: "))
+#else:
     # Si se pasó un argumento, se toma ese valor
-    num = int(sys.argv[1])
+    #num = int(sys.argv[1])
 
 # Se muestra el resultado del factorial
-print("Factorial ", num, "! es ", factorial(num))
+#print("Factorial ", num, "! es ", factorial(num))
+
+
+# Obtengo el valor (puede ser número o rango)
+if len(sys.argv) < 2:
+    valor = input("Ingrese un número o rango: ")
+else:
+    valor = sys.argv[1]
+
+# Verifico si es un rango
+if "-" in valor:
+    # Separo los valores del rango
+    partes = valor.split("-")
+    
+    desde = int(partes[0])
+    hasta = int(partes[1])
+
+    # Recorro el rango y calculo factorial de cada número
+    for i in range(desde, hasta + 1):
+        print("Factorial ", i, "! es ", factorial(i))
+
+else:
+    # Si es un número, lo convierto a entero
+    num = int(valor)
+    
+    # Muestro el resultado
+    print("Factorial ", num, "! es ", factorial(num))
